5c3351fa18f4813c0dab7f826af03646aad7a036
Valentina Kunina <konfeta333@gmail.com>
2020-02-20 12:29:28 +0300

    Домашнее задание к Уроку 1

---
+++ Lesson 1/script.js
@@ -0,0 +1,13 @@
+// Домашнее задание к Уроку 1
+
+let message = prompt('Введите градусы по Цельсию: ');
+let result = (9 / 5) * parseInt(message) + 32;
+alert(`Температура по Фаренгейту равна: ${result}`);
+
+let name = 'Василий';
+let admin = name;
+alert(admin);
+
+let a = 1000;
+let b = '108';
+alert(a+b);
